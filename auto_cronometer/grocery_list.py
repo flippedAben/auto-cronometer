9b232218faf73ad88496a6e58acbc17203cdcd27
Ben Yang <ben.yang@utexas.edu>
2021-08-15 12:34:43 -0500

    Convert to friendly units (not grams)

--- auto_cronometer/grocery_list.py
+++ auto_cronometer/grocery_list.py
@@ -4,12 +4,16 @@ from pprint import pprint
 
 def get_grocery_list(locked_recipes_yaml):
     with open(locked_recipes_yaml, 'r') as f:
-        recipes = yaml.load(f, Loader=yaml.Loader)
+        locked_recipes = yaml.load(f, Loader=yaml.Loader)
 
+    recipes = locked_recipes['recipes']
     ingredients = consolidate_recipes(recipes)
-    pprint(ingredients)
 
-    return []
+    grams_per_unit = locked_recipes['grams_per_unit']
+    convert_units(ingredients, grams_per_unit)
+
+    pprint(ingredients)
+    return ingredients
 
 
 def consolidate_recipes(recipes):
@@ -22,10 +26,44 @@ def consolidate_recipes(recipes):
     ingredients = {}
     for recipe in recipes:
         for ingredient in recipe['ingredients']:
+            i = ingredient['id']
             name = ingredient['name']
             grams = ingredient['grams']
-            if name in ingredients:
-                ingredients[name] += grams
+            if i in ingredients:
+                ingredients[i]['grams'] += grams
             else:
-                ingredients[name] = grams
+                ingredients[i] = {
+                    'name': name,
+                    'grams': grams,
+                }
     return ingredients
+
+
+def convert_units(ingredients, grams_per_unit):
+    """
+    Convert the amount of each ingredient (in grams) to a friendlier unit.
+
+    Assume that "friendly" means the fewest digits. For example, 3 lb is
+    friendlier that 48 oz. Note: 16 oz is 1 lb.
+    """
+
+    for i in ingredients:
+        ingredient = ingredients[i]
+        grams_per_unit[i]['lb'] = grams_per_unit[i]['oz'] * 16
+
+        # Default to grams
+        friendliest_unit = 'g'
+        amount = float(ingredient['grams'])
+        min_digits = len(str(amount))
+        for unit, ratio in grams_per_unit[i].items():
+            # Get rid of insane rounding errors
+            unit_amount = round(ingredient['grams'] / ratio, 5)
+            digits = len(str(unit_amount))
+            if 0.1 <= unit_amount and digits < min_digits:
+                friendliest_unit = unit
+                amount = unit_amount
+                min_digits = digits
+        ingredient.pop('grams')
+        ingredient['unit'] = friendliest_unit
+        ingredient['amount'] = amount
+
